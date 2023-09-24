236622a51959386ef3dfc51062cc9317c5d47a14
Ayyushhh <ayushj820@gmail.com>
2023-09-25 01:54:44 +0530

    Working More on JS part

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
+// Header Main Page Details
+
+const userData = {
+        title: "Developer",
+        firstName: "Ayush",
+        lastName: "Jain",
+        country: "India",
+        aboutImage: "images/user.png"
+};
+
+const headerIdData = ['title', 'firstName', 'lastName', 'country'];
+
+headerIdData.forEach(elementId => {
+    const element = document.getElementById(elementId);
+    element.innerHTML = userData[elementId];
+});
+
+
+
+// About me Details 
+
 let desc = "Hello, I'm Ayush Jain, a frontend developer, 3D artist, UI/UX designer, and graphic designer with a versatile skill set. With a deep understanding of both the technical intricacies of game development and the creative nuances of 3D art and graphic design, I bring a holistic approach that fosters innovation and ensures the seamless integration of aesthetics and functionality in every project.";
 document.getElementById("desc").innerHTML = desc;
 
+const userPhoto = document.getElementById('about-image');
+userPhoto.src = userData.aboutImage;
 
 
-var tablinks = document.getElementsByClassName("tab-links");
-        var tabcontents = document.getElementsByClassName("tab-contents");
 
-        function opentab(tabname){
-            for(tablink of tablinks){
-                tablink.classList.remove("active-link");
-            }
 
-            for(tabcontent of tabcontents){
-                tabcontent.classList.remove("active-tab");
-            }
-
-            event.currentTarget.classList.add("active-link");
-            document.getElementById(tabname).classList.add("active-tab");
-        }
 
 
 var sidemenu = document.getElementById("sidemenu");
-        function openmenu(){
-            sidemenu.style.right = "0";
-        }
+function openmenu(){
+    sidemenu.style.right = "0";
+}
 
-        function closemenu(){
-            sidemenu.style.right = "-200px";
-        }
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
 
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwPo5tZTgyeIwaXV19D2-SvwmdVQa_w2HWCbg4Bvw14D4TLS4n1YuP-R4TNC9GCakEF7Q/exec'
